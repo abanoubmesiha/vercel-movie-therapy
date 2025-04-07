8d670bcdb846c1bb421df9bf6edcdf3f2594326e
Abanoub Nagy <bony.nagy@gmail.com>
2025-04-07 05:22:32 +0200

    feat: add login titles for SEO and increase width

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { toast } from '@/components/toast';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useActionState, useEffect, useState } from 'react';
-import { toast } from '@/components/toast';
 
 import { AuthForm } from '@/components/auth-form';
 import { SubmitButton } from '@/components/submit-button';
@@ -47,9 +47,10 @@ export default function Page() {
 
   return (
     <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
-      <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12">
+      <div className="w-full max-w-xl overflow-hidden rounded-2xl flex flex-col gap-12">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
-          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>
+          <h1 className="text-xl font-semibold dark:text-zinc-50">Get Smarter Movie Recommendations</h1>
+          <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In to Personalize Your Mood-Based Picks!</h3>
           <p className="text-sm text-gray-500 dark:text-zinc-400">
             Use your email and password to sign in
           </p>
