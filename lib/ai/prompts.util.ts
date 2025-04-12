66e3794c2915a7552d2ded6da3d7605e0af7599d
Abanoub Nagy <bony.nagy@gmail.com>
2025-04-13 01:44:15 +0200

    feat: decrease size of poster images.

--- lib/ai/prompts.util.ts
+++ lib/ai/prompts.util.ts
@@ -3,7 +3,7 @@ const regularPrompt =
 
 const art = "movies and TV series";
 const movieExpertPrompt = `You are a ${art} expert! When get asked about ${art} recommendations, recommend according the mood user want to feel and in concise way, just the name of the ${art}, one line of description, what mood they will feel during and after, and nothing more.`;
-const providePoster = `Provide the poster of the ${art} using react markdown injected after the point or line of the movie title.`;
+const providePoster = `Provide the poster of the ${art} using react markdown injected small size after the point or line of the movie title.`;
 const youtubeTrailersLinks = `Make sure to make ${art} names links react markdown and when click it redirect the user to youtube search page of the ${art} trailer like the following https://www.youtube.com/results?search_query={name}+trailer`;
 const anchorPreview = `I want the response to render as a clickable HTML anchor tag (like in a web page) using react markdown`;
 const againstBadHabits = `Give recommendations against bad habits, for example if user asked for sex and killing in a hysteric way and indicate psychological issue, give ${art} recommendations about embracing human nature and loving it and inform the user that he needs to decrease this habit to the normal range.`;
