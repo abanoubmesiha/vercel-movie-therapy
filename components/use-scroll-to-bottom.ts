d2bb8ad47f3c03273b95bfc93f92d1e87ea08050
Abanoub Nagy <bony.nagy@gmail.com>
2025-04-14 00:26:27 +0200

    feat: allow public chat display and scrolled to top

--- components/use-scroll-to-bottom.ts
+++ components/use-scroll-to-bottom.ts
@@ -1,19 +1,22 @@
-import { useEffect, useRef, type RefObject } from 'react';
+import { useSearchParams } from "next/navigation";
+import { useEffect, useRef, type RefObject } from "react";
 
 export function useScrollToBottom<T extends HTMLElement>(): [
   RefObject<T>,
-  RefObject<T>,
+  RefObject<T>
 ] {
   const containerRef = useRef<T>(null);
   const endRef = useRef<T>(null);
+  const searchParams = useSearchParams();
+  const isAutoBottomScrollDisabled = searchParams.get("isAutoBottomScrollDisabled");
 
   useEffect(() => {
     const container = containerRef.current;
     const end = endRef.current;
 
-    if (container && end) {
+    if (container && end && isAutoBottomScrollDisabled !== "true") {
       const observer = new MutationObserver(() => {
-        end.scrollIntoView({ behavior: 'instant', block: 'end' });
+        end.scrollIntoView({ behavior: "instant", block: "end" });
       });
 
       observer.observe(container, {
@@ -25,7 +28,7 @@ export function useScrollToBottom<T extends HTMLElement>(): [
 
       return () => observer.disconnect();
     }
-  }, []);
+  }, [isAutoBottomScrollDisabled]);
 
   return [containerRef, endRef];
 }
