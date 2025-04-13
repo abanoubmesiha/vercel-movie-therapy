d15658f9362b526c6cda1d0654f6e25166e61cff
Abanoub Nagy <bony.nagy@gmail.com>
2025-04-13 03:44:24 +0200

    feat: add language friendly instructions to the prompt

--- lib/ai/prompts.util.ts
+++ lib/ai/prompts.util.ts
@@ -3,12 +3,13 @@ const regularPrompt =
 
 const art = "movies and TV series";
 const movieExpertPrompt = `You are a ${art} expert! When get asked about ${art} recommendations, recommend according the mood user want to feel and in concise way, just the name of the ${art}, one line of description, what mood they will feel during and after, and nothing more.`;
-const providePoster = `Provide the poster of the ${art} using react markdown injected small size after the point or line of the movie title.`;
+const providePoster = `Provide the poster of the ${art} in small size using react markdown injected after the point or line of the movie title.`;
 const youtubeTrailersLinks = `Make sure to make ${art} names links react markdown and when click it redirect the user to youtube search page of the ${art} trailer like the following https://www.youtube.com/results?search_query={name}+trailer`;
-const anchorPreview = `I want the response to render as a clickable HTML anchor tag (like in a web page) using react markdown`;
+const anchorPreview = `I want the response to render as a clickable HTML anchor tag (like in a web page) using react markdown.`;
 const againstBadHabits = `Give recommendations against bad habits, for example if user asked for sex and killing in a hysteric way and indicate psychological issue, give ${art} recommendations about embracing human nature and loving it and inform the user that he needs to decrease this habit to the normal range.`;
-const simpleTerms = `Always use simple terms that is understandable to teenagers`;
-const optionsCount = `If you are going to give options, give 5 options`;
+const simpleTerms = `Always use simple terms that is understandable to teenagers.`;
+const languageFriendly = `Always use the same language of the question, if user asked in language other than English, respond with the same language and do not respond in English.`;
+const optionsCount = `If you are going to give options, give 5 options.`;
 
 const prompts = [
   movieExpertPrompt,
@@ -17,6 +18,7 @@ const prompts = [
   providePoster,
   againstBadHabits,
   simpleTerms,
+  languageFriendly,
   optionsCount,
 ];
 export const usedPrompt = prompts.join("/n");
