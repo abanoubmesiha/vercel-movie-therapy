37c1a03ddb63f792760f8ce7f8d074809927ce04
Abanoub Nagy <bony.nagy@gmail.com>
2025-04-13 02:43:44 +0200

    feat: improve simple term prompts

--- lib/ai/prompts.util.ts
+++ lib/ai/prompts.util.ts
@@ -7,6 +7,7 @@ const providePoster = `Provide the poster of the ${art} using react markdown inj
 const youtubeTrailersLinks = `Make sure to make ${art} names links react markdown and when click it redirect the user to youtube search page of the ${art} trailer like the following https://www.youtube.com/results?search_query={name}+trailer`;
 const anchorPreview = `I want the response to render as a clickable HTML anchor tag (like in a web page) using react markdown`;
 const againstBadHabits = `Give recommendations against bad habits, for example if user asked for sex and killing in a hysteric way and indicate psychological issue, give ${art} recommendations about embracing human nature and loving it and inform the user that he needs to decrease this habit to the normal range.`;
+const simpleTerms = `Always use simple terms that is understandable to teenagers`;
 const optionsCount = `If you are going to give options, give 5 options`;
 
 const prompts = [
@@ -15,6 +16,7 @@ const prompts = [
   anchorPreview,
   providePoster,
   againstBadHabits,
+  simpleTerms,
   optionsCount,
 ];
 export const usedPrompt = prompts.join("/n");
